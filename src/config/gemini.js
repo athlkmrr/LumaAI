e40bf28af6e97cad9b1e11251d4ec40baf62b8d7
athlkmrr <147309157+athlkmrr@users.noreply.github.com>
2024-10-18 17:41:44 +0530

    Add files via upload

---
+++ src/config/gemini.js
@@ -0,0 +1,33 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyBX6bGGhp2kgjy0Rd - yuOni6J3u8QWYR4M";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+  return result.response.text();
+}
+
+export default run;
